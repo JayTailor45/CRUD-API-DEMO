b141715f617749bf94fe7624221950ce23b09685
JayTailor45 <tailorj64@gmail.com>
2019-11-22 19:02:23 +0530

    init

---
+++ index.js
@@ -0,0 +1,18 @@
+// Import packages
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+// App
+const app = express();
+app.use(cors());
+// Morgan
+app.use(morgan('tiny'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(require('./routes/index.routes'));
+// First route
+app.get('/', (req, res) => {
+    res.json({ message: 'Hello world' })
+});
+// Starting server
+app.listen('1337');
